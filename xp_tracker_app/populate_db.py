26391012b5f1ce5c3093c8ebca5831ba5316103a
Justas Trimailovas <j.trimailovas@gmail.com>
2014-05-30 13:54:23 +0300

    Created populate_db script, for database population

---
+++ xp_tracker_app/populate_db.py
@@ -0,0 +1,18 @@
+from xp_tracker_app.models import Story, Task
+from django.utils import timezone
+import datetime as dt
+import random
+
+""" Module, for populating database """
+
+def create_stories():
+    for story in range(5):
+        Story.objects.create(story_name="User story #%s" % str(story+1),
+                             time_est=(timezone.now() + dt.timedelta(weeks=story+1)).date())
+
+def create_tasks():
+    for task in range(30):
+        Task.objects.create(task_name="Task #%s" % str(task+1),
+                            time_est=(timezone.now() + dt.timedelta(days=task+1)).date(),
+                            developer=Task.DEVELOPERS[random.randint(0, 4)][1],
+                            iteration=random.randint(1, 5))
