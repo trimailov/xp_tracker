6873034901f28f54a3655a8c0000ff081982af7f
Justas Trimailovas <j.trimailovas@gmail.com>
2014-05-31 17:01:53 +0300

    added test for models.delta_to_time

--- xp_tracker_app/test_unit.py
+++ xp_tracker_app/test_unit.py
@@ -1,27 +1,27 @@
 import datetime as dt
 from django.test import TestCase
 from django.utils import timezone
-from xp_tracker_app.models import Story, Task
+from xp_tracker_app.models import Story, Task, delta_to_time
 
 
 class StoryTestCase(TestCase):
     """ Unit tests for Story model """
     def setUp(self):
         Story.objects.create(story_name="User story",
-                             time_est=(timezone.now() + dt.timedelta(weeks=1)).date())
+                             time_est=dt.datetime(2015, 6, 1, 12))
 
     def test_story_creates(self):
         story = Story.objects.get(story_name="User story")
 
         self.assertEqual(story.story_name, "User story")
-        self.assertEqual(story.time_est, (timezone.now() + dt.timedelta(weeks=1)).date())
-        self.assertEqual(story.time_start, dt.date.today())
+        self.assertEqual(story.time_est.replace(tzinfo=None), dt.datetime(2015, 6, 1, 12))
+        self.assertEqual(story.time_start.date(), dt.date.today())
 
 class TaskTestCase(TestCase):
     """ Unit tests for Task model """
     def setUp(self):
         Task.objects.create(task_name="Task name",
-                            time_est=(timezone.now() + dt.timedelta(days=1)).date(),
+                            time_est=dt.datetime(2015, 6, 1, 12),
                             developer='Linus Torvalds',
                             iteration=1)
 
@@ -30,8 +30,8 @@ class TaskTestCase(TestCase):
         """ Tests new created task (finishing time should be None) """
         task1 = Task.objects.get(task_name="Task name")
 
-        self.assertEqual(task1.time_est, (timezone.now() + dt.timedelta(days=1)).date())
-        self.assertEqual(task1.time_start, dt.date.today())
+        self.assertEqual(task1.time_est.replace(tzinfo=None), dt.datetime(2015, 6, 1, 12))
+        self.assertEqual(task1.time_start.date(), dt.date.today()) 
         self.assertIsNone(task1.time_fin)
         self.assertEqual(task1.developer, 'Linus Torvalds')
         self.assertEqual(task1.iteration, 1)
@@ -39,6 +39,20 @@ class TaskTestCase(TestCase):
     def test_finished_task(self):
         """ Tests task finishing time comparing to estimated. """
         task1 = Task.objects.get(task_name="Task name")
-        task1.time_fin = (timezone.now() + dt.timedelta(days=1)).date()
+        task1.time_fin = dt.datetime(2015, 6, 1, 12)
 
-        self.assertEqual(task1.time_fin, task1.time_est)
+        self.assertEqual(task1.time_fin.replace(tzinfo=None), dt.datetime(2015, 6, 1, 12))
+
+class ModelsTestCase(TestCase):
+    """ Unit tests for extra functions in models.py """
+    def test_delta_to_time(self):
+        time1 = dt.timedelta(1) # 1 day
+        time2 = dt.timedelta(0, 1) # 1 sec
+        time3 = dt.timedelta(0, 60) # 1 min
+        time4 = dt.timedelta(0, 3600) # 1 hour
+
+        self.assertEqual(delta_to_time(time1), "1d 0h 0m 0s")
+        self.assertEqual(delta_to_time(time2), "0d 0h 0m 1s")
+        self.assertEqual(delta_to_time(time3), "0d 0h 1m 0s")
+        self.assertEqual(delta_to_time(time4), "0d 1h 0m 0s")
+        self.assertEqual(delta_to_time(time1+time2+time3+time4), "1d 1h 1m 1s")
