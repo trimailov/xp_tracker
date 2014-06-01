38624a237e68ecc0319c0d737b97c8704829231e
Justas Trimailovas <j.trimailovas@gmail.com>
2014-06-01 16:42:38 +0300

    Added tests for story and task form status code checking

--- xp_tracker_app/test_func.py
+++ xp_tracker_app/test_func.py
@@ -3,18 +3,19 @@ from django.test import TestCase
 from django.utils import timezone
 import datetime as dt
 from xp_tracker_app.models import Story
+from xp_tracker_app.forms import TaskForm
 
 class IndexTest(TestCase):
     """ Functional tests for index page """
     def setUp(self):
-        self.c = Client()
+        self.client = Client()
 
     def test_index_exists(self):
-        response = self.c.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_index_template(self):
-        response = self.c.get('/')
+        response = self.client.get('/')
 
         self.assertContains(response, 'Main page')
 
@@ -27,3 +28,18 @@ class IndexTest(TestCase):
             self.assertContains(response, 'Tasks')
         else:
             self.assertContains(response, 'There are no tasks')
+
+class FormTest(TestCase):
+    """ Functional tests for forms """
+    def setUp(self):
+        self.client = Client()
+
+    def test_task_form_opens(self):
+        response = self.client.get('/new_task/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'new_task.html', 'base.html')
+
+    def test_story_form_opens(self):
+        response = self.client.get('/new_story/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'new_story.html', 'base.html')
