518d8e855f3d9af501b8ecf27873a98653479e7e
Justas Trimailovas <j.trimailovas@gmail.com>
2014-06-01 16:09:42 +0300

    Test for TestFinsihingHistory. Tests if TestFinishingHistory points to the correct Task

--- xp_tracker_app/test_unit.py
+++ xp_tracker_app/test_unit.py
@@ -1,7 +1,7 @@
 import datetime as dt
 from django.test import TestCase
 from django.utils import timezone
-from xp_tracker_app.models import Story, Task
+from xp_tracker_app.models import Story, Task, TaskFinishingHistory
 from xp_tracker_app.helper import delta_to_time
 
 
@@ -24,7 +24,9 @@ class TaskTestCase(TestCase):
         Task.objects.create(task_name="Task name",
                             time_est=dt.datetime(2015, 6, 1, 12),
                             developer='Linus Torvalds',
-                            iteration=1)
+                            iteration=1,
+                            story=Story.objects.create(story_name="User story 2",
+                                                       time_est=dt.datetime(2015, 6, 1, 12)))
 
 
     def test_new_task_creates(self):
@@ -43,6 +45,43 @@ class TaskTestCase(TestCase):
         task1.time_fin = dt.datetime(2015, 6, 1, 12)
 
         self.assertEqual(task1.time_fin.replace(tzinfo=None), dt.datetime(2015, 6, 1, 12))
+
+class TaskFinishingHistoryTestCase(TestCase):
+    """ Unit tests for TaskFinishingHistory model """
+    def setUp(self):
+        story = Story.objects.create(story_name="User story 3",
+                                     time_est=dt.datetime(2015, 6, 1, 12))
+        task1 = Task.objects.create(task_name="Task name 1",
+                            time_est=dt.datetime(2015, 1, 1, 12),
+                            developer='Guido von Rossum',
+                            iteration=1,
+                            story=story)
+        task2 = Task.objects.create(task_name="Task name 2",
+                            time_est=dt.datetime(2015, 1, 1, 12),
+                            developer='Linus Torvalds',
+                            iteration=2,
+                            story=story)
+        TaskFinishingHistory.objects.create(task_id=task1.id, time_fin=timezone.now())
+        TaskFinishingHistory.objects.create(task_id=task2.id, time_fin=timezone.now())
+        TaskFinishingHistory.objects.create(task_id=task1.id, 
+                                            time_fin=timezone.now()+dt.timedelta(weeks=1))
+        TaskFinishingHistory.objects.create(task_id=task2.id, 
+                                            time_fin=timezone.now()+dt.timedelta(days=1))
+
+    def test_task_multi_finish(self):
+        """ Test if TaskFinishingHistory is pointing to the correct Task """
+        task1 = Task.objects.get(task_name="Task name 1")
+        task2 = Task.objects.get(task_name="Task name 2")
+
+        task1_history = task1.taskfinishinghistory_set.all()
+        task2_history = task2.taskfinishinghistory_set.all()
+
+        for entry1 in task1_history:
+            self.assertEqual(entry1.task_id, task1.id)
+
+        for entry2 in task2_history:
+            self.assertEqual(entry2.task_id, task2.id)
+
 
 class HelpersTestCase(TestCase):
     """ Unit tests for helper functions in helper.py """
